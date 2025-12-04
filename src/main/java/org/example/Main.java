cb9af8991d766b8d9d77634a81f5a5687341e3cb
StevenLRDev <stevenzakuro4598@gmail.com>
2025-12-04 15:53:08 -0500

    feat: se crea estructura inicial del proyecto y clase Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        //desarrolla el codigo aca
+        Scanner leerTeclado = new Scanner(System.in);
 
     }
 }
+
