89585fff7fbe3e1dcf19b14d9a5601f8fb840421
Juan Jose Gomez <juanjosexg23@gmail.com>
2025-12-04 14:50:01 -0500

    Comentarios con funciones para trabajo conlectivo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        //App desarrollada por
+        // DAVID HENAO MONCADA
+        // JUAN JOSE GOMEZ
 
-        //desarrolla el codigo aca
+        //E-COMMERCE APLICATIVO DIGITAL COMERCE J&D
+
+        //FUNCIONES
+        //Funcion para calcular el costo total de la venta (Subtotal)
+
+        //Funcion para aplicar impuestos
+
+        //Funcion para aplicar descuentos
+
+        //Funcion para calcular costo de encios según distancia
+
+        //Funcion para calcular el total final de la compra
+
+
 
     }
 }
