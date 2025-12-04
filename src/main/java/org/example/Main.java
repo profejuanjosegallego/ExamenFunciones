f3f0f41aa23db989327185852ed04d4ab283510f
StevenLRDev <stevenzakuro4598@gmail.com>
2025-12-04 15:57:50 -0500

    feat: se agrega arraylist de productos y se almacenan los datos ingresados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,24 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner leerTeclado = new Scanner(System.in);
+        //
+        // Colores para UX
+        String GREEN = "\u001B[32m";
 
+        // Título del e-commerce
+        System.out.println(GREEN + "\n-----NEVADA COMMERCE -----");
+        System.out.println(GREEN + "===BY: STEVEN & ADRIANA'S CORP===");
+        System.out.println("Calculamos tu compra paso a paso...\n");
+        System.out.println("*********************************");
+
+        // Lista donde guardaré todos los productos
+        ArrayList<HashMap<String, Object>> productos = new ArrayList<>();
+
+        // Aquí permito al usuario agregar varios productos
+        System.out.print("¿Cuántos productos desea registrar?: ");
+        Integer cantidadProductosLista = leerTeclado.nextInt();
+        leerTeclado.nextLine(); // Limpia buffer
     }
 }
+
 
